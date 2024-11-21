ee2f575d6b630287566991f361e81d395a8dd21a
BiBiKo219 <75228969+BiBiKo219@users.noreply.github.com>
2024-11-21 22:16:24 +0800

    mmtrack_v2

---
+++ mmtrack/models/roi_heads/bbox_heads/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .selsa_bbox_head import SelsaBBoxHead
+from .single_bbox_head import SingleBBoxHead
+
+__all__ = ['SelsaBBoxHead', 'SingleBBoxHead']
